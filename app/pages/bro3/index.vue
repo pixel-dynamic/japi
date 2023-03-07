e7b0e4817b3406071f22e38ec54ed550224074b0
Christian <cmendieta@lumbrera.com.mx>
2023-03-06 19:22:47 -0600

    add landings brother

---
+++ app/pages/bro3/index.vue
@@ -0,0 +1,103 @@
+<template>
+  <div class="w-100">
+    <table width="100%" border="0" cellpadding="0" cellspacing="0">
+      <tr>
+        <td></td>
+        <td width="640">
+          <br />
+
+          <table
+            id="Tabla_01"
+            width="100%"
+            border="0"
+            cellpadding="0"
+            cellspacing="0"
+          >
+            <tr>
+              <td>
+                <a
+                  target="_blank"
+                  href="https://afredar.com/?a=1988733&c=15452&p=c&s1=SN-SuperNova_MX-BD"
+                >
+                  <img
+                    src="img_07.jpg"
+                    style="
+                      width: 100%;
+                      margin: 0px;
+                      padding: 0px;
+                      border: 0px;
+                      display: block;
+                    "
+                    alt="brother"
+                /></a>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <a
+                  target="_blank"
+                  href="https://afredar.com/?a=1988733&c=15452&p=c&s1=SN-SuperNova_MX-BD"
+                >
+                  <img
+                    src="img_08.jpg"
+                    style="
+                      width: 100%;
+                      margin: 0px;
+                      padding: 0px;
+                      border: 0px;
+                      display: block;
+                    "
+                    alt="brother"
+                /></a>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <a
+                  target="_blank"
+                  href="https://afredar.com/?a=1988733&c=15452&p=c&s1=SN-SuperNova_MX-BD"
+                >
+                  <img
+                    src="img_09.jpg"
+                    style="
+                      width: 100%;
+                      margin: 0px;
+                      padding: 0px;
+                      border: 0px;
+                      display: block;
+                    "
+                    alt="brother"
+                /></a>
+              </td>
+            </tr>
+            <tr>
+              <td
+                style="
+                  text-align: center;
+                  font-family: Arial, Helvetica, sans-serif;
+                  font-size: 13px;
+                  color: #333;
+                  padding: 1%;
+                "
+              >
+                <p>brother</p>
+              </td>
+            </tr>
+          </table>
+        </td>
+        <td></td>
+      </tr>
+    </table>
+    <img
+      src="https://i.afiliacion.net/?a=1988733&c=15452&p=c&E=NnDvcmS9SstExBMjfyAJvjr4D79rUCrF&s1=SN-SuperNova_MX-BD"
+      width="1"
+      height="1"
+      border="0"
+    />
+  </div>
+</template>
+<script>
+export default {
+  layout: 'brother',
+}
+</script>
