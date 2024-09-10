4a0c598baa0e2a16a6e63f3aabdf0398aca110dd
Víctor Ludwig Escalante Núñez <victorlt.xd3@gmail.com>
2024-09-10 12:20:10 -0600

    Add cors

--- app/api/element.js
+++ app/api/element.js
@@ -4,6 +4,18 @@ import bodyParser from 'body-parser';
 
 // Asegúrate de procesar el cuerpo de la solicitud con body-parser
 export default (req, res) => {
+
+  res.setHeader('Access-Control-Allow-Origin', '*'); // O reemplazar '*' con 'https://japiapp.com.mx' para mayor seguridad
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+  // Si es una solicitud OPTIONS (preflight), responde con un 200 y termina
+  if (req.method === 'OPTIONS') {
+    res.statusCode = 200;
+    res.end();
+    return;
+  }
+
   bodyParser.json()(req, res, async () => {
     const API_KEY = '51bf211e-d68e-4a2c-af8e-d29d12173c3d'; // Reemplaza con tu clave API
     const LIST_ID = '84e54bf2-6f1a-11ef-8864-1fff41337fd8'; // Reemplaza con el ID de tu lista
